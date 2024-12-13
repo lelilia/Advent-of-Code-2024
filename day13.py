c69e1641f3b5a7c27fb654f4e1342a74ec847ef7
lelilia <lelilia@posteo.de>
2024-12-13 16:55:28 +0100

    day 13

--- day13.py
+++ day13.py
@@ -2,32 +2,15 @@ import re
 
 
 def get_game_parameters(game):
-    game_input = re.findall(
-        r"Button A: X([+-]\d+), Y([+-]\d+)\nButton B: X([+-]\d+), Y([+-]\d+)\nPrize: X=(\d+), Y=(\d+)",
-        game,
-    )
-    return [int(x) for x in game_input[0]]
+    return [int(x) for x in re.findall(r"[+-]?\d+", game)]
 
 
-def part_1(puzzle):
+def solve(puzzle, offset=0):
     res = 0
     for game in puzzle.split("\n\n"):
-        game_params = get_game_parameters(game)
-        [a_x, a_y, b_x, b_y, p_x, p_y] = game_params
-        a = (p_x * b_y - p_y * b_x) / (a_x * b_y - a_y * b_x)
-        b = (p_x - a * a_x) / b_x
-        if a % 1 == 0 and b % 1 == 0 and a <= 100 and b <= 100:
-            res += 3 * int(a) + int(b)
-    return res
-
-
-def part_2(puzzle):
-    res = 0
-    for game in puzzle.split("\n\n"):
-        game_params = get_game_parameters(game)
-        [a_x, a_y, b_x, b_y, p_x, p_y] = game_params
-        p_x += 10000000000000
-        p_y += 10000000000000
+        [a_x, a_y, b_x, b_y, p_x, p_y] = get_game_parameters(game)
+        p_x += offset
+        p_y += offset
         a = (p_x * b_y - p_y * b_x) / (a_x * b_y - a_y * b_x)
         b = (p_x - a * a_x) / b_x
         if a % 1 == 0 and b % 1 == 0:
@@ -38,5 +21,5 @@ def part_2(puzzle):
 with open("input13") as f:
     puzzle = f.read()
 
-print("Part 1:", part_1(puzzle))
-print("Part 2:", part_2(puzzle))
+print("Part 1:", solve(puzzle))
+print("Part 2:", solve(puzzle, offset=10000000000000))
